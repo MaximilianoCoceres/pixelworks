51b5ce76e5566f1c1b8ea33fbe6cda2915d27c52
MaximilianoCoceres <maximilianococeres7@gmail.com>
2023-05-11 15:40:43 +0200

    guardo por las dudas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,25 +2,37 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ServiciosComponent } from './servicios/servicios.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     InicioComponent,
     HeaderComponent,
-    ServiciosComponent
+    ServiciosComponent,
+    PortfolioComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+
+
   ],
-  providers: [],
+  exports:[],
+  providers: [
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
